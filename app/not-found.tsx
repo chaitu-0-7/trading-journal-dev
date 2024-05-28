ee836fd83eda2b6c9e53dd7eb089b754d0a495b6
Yadavalli Chaitanya <ychaitu2@gmail.com>
2024-05-29 02:24:06 +0530

    design changed error page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div className="not-found flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="not-found flex flex-col items-center justify-center min-h-screen">
       <h2 className="text-4xl font-bold text-center mb-4">Not Found</h2>
       <p className="text-xl text-center mb-6">The page you are looking for is not present</p>
       <Link href="/">
